aa2064bab733255d032236a21d6233cfb9620eef
vladrain27 <radu.vladc@gmail.com>
2023-03-09 12:19:13 +0200

    Homework for 1st session

---
+++ CSharpTraining/Program.cs
@@ -0,0 +1,11 @@
+﻿int BoxA = 5, BoxB = 10;
+int sum = BoxA + BoxB;
+int dif = BoxA - BoxB;
+int multipli = BoxA * BoxB;
+int division = BoxA % BoxB;
+Console.WriteLine(BoxA + " + " + BoxB + " = " + sum);
+Console.WriteLine(BoxA + " - " + BoxB + " = " + dif);
+Console.WriteLine(BoxA + " * " + BoxB + " = " + multipli);
+Console.WriteLine(BoxA + " % " + BoxB + " = " + division);
+Console.WriteLine("ta daa");
+Console.ReadLine();
